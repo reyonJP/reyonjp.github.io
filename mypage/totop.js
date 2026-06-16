803c218ca13834757b3f838f4de0eedfc11367d1
reyonJP <keidec25@gmail.com>
2026-06-16 21:15:00 +0900

    mypage移動

---
+++ mypage/totop.js
@@ -0,0 +1,10 @@
+document.write(
+    `
+    <div class="totop">
+        <a href="#">
+            <p>PAGE TOP</p>
+            <img src="icon.png" width="120" height="100">
+        </a>
+    </div>
+    `
+);
